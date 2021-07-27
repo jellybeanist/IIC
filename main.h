810b2a9f8758be6166572e77688e0d79f6ac7d91
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-07-27 10:02:15 +0300

    Update main.h
    QMC5883 library definitions added.

--- main.h
+++ main.h
@@ -19,14 +19,38 @@
 #define TX_BUF_FULL 	((u32*) (BRAM_READ_BASE_ADDRESS + 4*(0x02)))
 
 /*
- * For MCP4725:
+ * For MCP4725 => 12 Bit DAC :
  */
-#define MCP4725_DEV_ADDRESS					    0x62 //A0 is logic zero, wired to the ground. 0x63 if its logic 1.
-#define MCP4725_WRITEDAC_ADDRESS			  0x40
-#define MCP4725_WRITEDACEEPROM_ADDRESS	0x60
+#define MCP4725_DEV_ADDRESS					      0x62 //A0 is logic zero, wired to the ground. 0x63 if its logic 1.
+#define MCP4725_WRITEDAC_ADDRESS			    0x40
+#define MCP4725_WRITEDACEEPROM_ADDRESS		0x60
 
-XStatus i2cAD7414Write(u8 regAddr, u8 data);
-XStatus i2cAD7414Read(u8 regAddr, u8* dataPtr, u8 dataCount);
+/*
+ * For GY-231 (QMC5883) => 3 axis compass :
+ * => Temperature Output Registers (07H and 08H).
+ * => Mode register (09H).
+ */
+
+#define QMC5883_DEV_ADDRESS					0x0D //Device address (0001101)
+#define QMC5883_X_AXIS_LSB					0x00 //X axis information (LSB)
+#define QMC5883_X_AXIS_MSB					0x01 //X axis information (MSB)
+#define QMC5883_Y_AXIS_LSB					0x02 //Y axis information (LSB)
+#define QMC5883_Y_AXIS_MSB					0x03 //Y axis information (MSB)
+#define QMC5883_Z_AXIS_LSB					0x04 //Z axis information (LSB)
+#define QMC5883_Z_AXIS_MSB					0x05 //Z axis information (MSB)
+#define QMC5883_TEMPERATURE_LSB			0x07 //Temperature value (LSB)
+#define QMC5883_TEMPERATURE_MSB			0x08 //Temperature value (MSB)
+#define QMC5883_WRITE_ADDRESS				0x09 //Temperature value (MSB)
+
+
+
+
+XStatus IIC_MCP4725_Write(u8 regAddr, u8 data0, u8 data1);
+XStatus IIC_MCP4725_Read(u8 regAddr, u8* dataPtr, u8 dataCount);
+
+XStatus IIC_QMC5883_Write(u8 regAddr, u8 slaveAddr, u8 data);
+XStatus IIC_QMC5883_Read(u8 regAddr, u8* dataPtr, u8 dataCount);
+
 
 #endif
 
