f107e8847a3efdc4fba342c20f99e8302ed17a14
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-08-02 17:24:02 +0300

    Update main.c
    Header parsing.

--- main.c
+++ main.c
@@ -6,213 +6,125 @@
 #include <inttypes.h>
 #include <stdint.h>
 // device slave addr, r/w,
-u32 temp=0;
-u32 adder=0;
-u32 right=0;
-u32 shift=0;
-u32 sent=0;
-
-static int IIC_SensorInitialized = FALSE ;
-
-void IIC_Initialize(void)
-{
-	IIC_SensorInitialized = TRUE;
-}
-
-void i2cFree(void)
-{
-	IIC_SensorInitialized = FALSE ;
-}
-
-
-XStatus IIC_MCP4725_Write(u8 regAddr, u8 data0, u8 data1)
-{
-	u8 wrData[2];
-
-	wrData[0] = regAddr;
-	wrData[0] = data0;
-	wrData[1] = data1;
-
-	/* register write single byte */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, MCP4725_DEV_ADDRESS, wrData, sizeof(wrData), XIIC_STOP) != sizeof(wrData))
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-XStatus IIC_MCP4725_Read(u8 regAddr, u8* dataPtr, u8 dataCount)
-{
-	/* register select */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, MCP4725_DEV_ADDRESS, &regAddr, 1, XIIC_STOP) != 1)
-	{
-		return XST_FAILURE;
-	}
-	/* register read */
-	if(XIic_Recv(I2C_DEVICE_BASE_ADDR, MCP4725_DEV_ADDRESS, dataPtr, dataCount, XIIC_STOP) != dataCount)
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-XStatus IIC_QMC5883_Write(u8 regAddr, u8 slaveAddr, u8 data)
-{
-	u8 wrData[3];
-
-	wrData[0] = regAddr;
-	wrData[1] = slaveAddr;
-	wrData[2] = data;
-
-	/* register write single byte */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, QMC5883_DEV_ADDRESS, wrData, sizeof(wrData), XIIC_STOP) != sizeof(wrData))
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-XStatus IIC_QMC5883_Read(u8 regAddr, u8* dataPtr, u8 dataCount)
-{
-	/* register select */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, QMC5883_DEV_ADDRESS, &regAddr, 1, XIIC_STOP) != 1)
-	{
-		return XST_FAILURE;
-	}
-	/* register read */
-	if(XIic_Recv(I2C_DEVICE_BASE_ADDR, QMC5883_DEV_ADDRESS, dataPtr, dataCount, XIIC_STOP) != dataCount)
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-XStatus IIC_MPU6050_Write(u8 regAddr, u8 data)
-{
-	u8 wrData[2];
-
-	wrData[0] = regAddr;
-	wrData[1] = data;
-
-	/* register write single byte */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, MPU6050_DEV_ADDRESS, wrData, sizeof(wrData), XIIC_STOP) != sizeof(wrData))
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-XStatus IIC_MPU6050_Read(u8 regAddr, u8* dataPtr, u8 dataCount)
-{
-	/* register select */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, MPU6050_DEV_ADDRESS, &regAddr, 1, XIIC_STOP) != 1)
-	{
-		return XST_FAILURE;
-	}
-	/* register read */
-	if(XIic_Recv(I2C_DEVICE_BASE_ADDR, MPU6050_DEV_ADDRESS, dataPtr, dataCount, XIIC_STOP) != dataCount)
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-
-XStatus IIC_PCF8574_Write(u8 regAddr, u8 data)
-{
-	u8 wrData[2];
-
-	wrData[0] = regAddr;
-	wrData[1] = data;
-
-	/* register write single byte */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, PCF8574_DEV_ADDRESS, wrData, sizeof(wrData), XIIC_STOP) != sizeof(wrData))
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
-
-
-XStatus IIC_PCF8574_Read(u8 regAddr, u8* dataPtr, u8 dataCount)
-{
-	/* register select */
-	if(XIic_Send(I2C_DEVICE_BASE_ADDR, PCF8574_DEV_ADDRESS, &regAddr, 1, XIIC_STOP) != 1)
-	{
-		return XST_FAILURE;
-	}
-	/* register read */
-	if(XIic_Recv(I2C_DEVICE_BASE_ADDR, PCF8574_DEV_ADDRESS, dataPtr, dataCount, XIIC_STOP) != dataCount)
-	{
-		return XST_FAILURE;
-	}
-	return XST_SUCCESS;
-}
 
 int main(void)
 {
-	u8 datas[8];
 	CLK_DIV_BAUD[0] = 100000000/115200;
 	sleep(1);
-	IIC_MCP4725_Write(0x40,0x0F,0xFF);
-	// int counter=0;
-	// u8 voltageLow,voltageHigh=0x00;
-	//IIC_MCP4725_Write(0x40,0x00,0x00);  //0F FF => 3.3V, second byte is for LSB.
-	while(1)
+
+	// HEADER: 	5 bytes. => 2 bytes initial + 1 byte protocol select + 2 bytes protocol message size.
+	// Protocol selection: 00 for UART, 01 for RS232, 02 for RS422, 03 for IIC, 04 for SPI.
+
+	// UART: 	16 bytes. => 1 byte enable + 4 bytes Baud Rate + 1 byte UART message size + 10 bytes UART message.
+	// RS232: 	16 bytes. => 1 byte enable + 4 bytes Baud Rate + 1 byte UART message size + 10 bytes UART message.
+	// RS422: 	16 bytes. => 1 byte enable + 4 bytes Baud Rate + 1 byte UART message size + 10 bytes UART message.
+
+	// IIC:
+
+	// SPI:
+
+	int header_data_counter = 0; // after receiving first 8 bits of the package, this variable should be zeroed.
+	int data_counter,message_counter=0;
+	int *message_array_ptr;
+	u16 message_size=0;
+	u8 rxdata;
+	//u64 test_data = 0xAA55BBCCDDEEFF99;
+	u8 header_specs[5];
+	//u8 test_array[18];
+	u8 check_sum = 0; // should be zeroed after getting the whole package.
+
+	while (1)
 	{
-		if  (IIC_PCF8574_Write(0x4E,0xCC) == XST_FAILURE)
-			//(IIC_MCP4725_Read(0x65,datas,2) == XST_FAILURE)
-			//(IIC_MCP4725_Write(0x40,0x0F,0xFF) == XST_FAILURE)
-			//(IIC_MPU6050_Read(0x42, datas, 1) == XST_FAILURE)
+		//rxdata = RX_BUF_DATA[0];
+//https://www.programiz.com/c-programming/c-dynamic-memory-allocation
+		while((RX_BUF_EMPTY[0] == 0)){
+			//TX_BUF_DATA[0] = RX_BUF_DATA[0];
+			if(header_data_counter<5)
+			{
+			header_specs[header_data_counter] = RX_BUF_DATA[0];
+			header_data_counter = header_data_counter + 1;
+			}
+			else if(header_data_counter==5)
+			{
+			message_size = (header_specs[3] << 8) + header_specs[4];
+			message_array_ptr = (int*) malloc( (int)message_size * sizeof(int) );
+			}
+			//message[(int)message_size];
+			//message[message_size] = RX_BUF_DATA[0];
+			else if(message_counter < (int)message_size)
+			{
+			(message_array_ptr) + message_counter == RX_BUF_DATA[0];
+			TX_BUF_DATA[0] = *message_array_ptr + message_counter;
+			message_counter = message_counter + 1;
+
+			}
+		}
+		/*if(data_counter2 == 18){
+		data_counter2=0;
+		TX_BUF_DATA[0] = test_array[5];
+		}*/
+
+		if(RX_BUF_EMPTY[0] == 0)
 		{
-			TX_BUF_DATA[0] = 0xAA;
+			//TX_BUF_DATA[0] = rxdata;
+			//TX_BUF_DATA[0] = RX_BUF_DATA[0];
+
+			if(data_counter == 0 && rxdata == 0xAA)
+			{
+				header_specs[data_counter] = rxdata;
+				data_counter = data_counter + 1;
+				//TX_BUF_DATA[0] = header_specs[0];
+			}
+
+			else if(data_counter == 1 && header_specs[0] == 0xAA)
+			{
+				header_specs[data_counter] = rxdata;
+				data_counter = data_counter + 1;
+				//TX_BUF_DATA[0] = header_specs[1];
+			}
+
+			else if (header_specs[0] == 0xAA && header_specs[1] == 0x55 && data_counter < 8)
+			{
+				header_specs[data_counter] = rxdata;
+				data_counter = data_counter + 1;
+
+				if(data_counter == 8)
+				{
+					//TX_BUF_DATA[0] = header_specs[0];
+					//TX_BUF_DATA[0] = header_specs[1];
+					//TX_BUF_DATA[0] = header_specs[2];
+					//TX_BUF_DATA[0] = header_specs[3];
+					//TX_BUF_DATA[0] = header_specs[4];
+					//TX_BUF_DATA[0] = header_specs[5];
+					//TX_BUF_DATA[0] = header_specs[6];
+					TX_BUF_DATA[0] = header_specs[7];
+					data_counter = 0;
+				}
+			}
+
+			else//control statement, will not be used later.
+				TX_BUF_DATA[0] = 0xBC;
+
 		}
 		else
 		{
-			//for (int i=0; i<2; i++)
-				//TX_BUF_DATA[0] = datas[0];
-			//TX_BUF_DATA[0] = datas[i];
-			TX_BUF_DATA[0] = 0xBB;
+			//TX_BUF_DATA[0] = 0xCB;
 		}
 
-		sleep(1);
+
+		/*if ([0] == 1)
+				CLK_DIV_BAUD_U0[0] = 100000000/1,2,3,4;
+				sleep(1);
+				TX_BUF_DATA_U0[0] = 5;
+		if([6] == 1)
+				CLK_DIV_BAUD_U1[0] = 100000000/7,8,9,10;
+				sleep(1);
+				TX_BUF_DATA_U1[0] = 11;
+				*/
+
+
 	}
-
-
-	//Write the data comes from UART to the 7 segment display.
-/*	while (1)
-	{
-		//IIC_MCP4725_Write(MCP4725_WRITEDAC_ADDRESS,voltageHigh,voltageLow);
-		if(RX_BUF_EMTY[0] == 0){
-			TX_BUF_DATA[0]=RX_BUF_DATA[0];
-			voltageHigh = RX_BUF_DATA[0];
-
-			if (counter==1){
-				voltageLow = RX_BUF_DATA[0];
-				IIC_MCP4725_Write(MCP4725_WRITEDAC_ADDRESS,voltageHigh,voltageLow);
-				counter=0;
-			}
-
-				counter=counter+1;
-
-			//SEVEN_SEGW[0] = shift+RX_BUF_DATA[0];
-
-
-
-			}
-		//shift = temp<<8;
-		}
-*/
-	//12 bit DAC function.
-	//IIC_MCP4725_Write(MCP4725_WRITEDAC_ADDRESS,0x00,0xFF);
-
-
-
-	return XST_SUCCESS;
+return 0;
 }
+
+//}
